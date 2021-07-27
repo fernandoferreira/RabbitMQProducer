a21ae919b192901fe599db5a354c369f13a1baad
fsferreira <fsferreira@LTMC020881>
2021-07-27 11:42:25 -0300

    Just a rename.

--- Program.cs
+++ Program.cs
@@ -13,15 +13,15 @@ namespace RabbitMQProducer
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "myqueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-                var message = "Hello Foundation";
+                var message = "Hello Foundation from RabbitMQ!";
                 var body = Encoding.UTF8.GetBytes(message);
 
                 channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
                 Console.WriteLine($"A mensagem {message} foi enviada.");
             }
-            
+
         }
     }
 }
